3667647b02828715c2e8bef2c6aba92ab50516ee
shrutichanda <shrchand0@publicisgroupe.net>
2019-04-30 12:51:17 +0530

    Please check for html chunks and dev server and css files

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,50 @@
  */
 
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
 		filename: 'bundle.[name]-[contenthash].js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader', // creates style nodes from JS strings
+					'css-loader', // translates CSS into CommonJS
+					'sass-loader' // compiles Sass to CSS, using Node Sass by default
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+      		chunkFilename: "[id].css"
+		})
+	]
 };
