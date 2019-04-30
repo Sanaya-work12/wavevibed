fd70e95d0d6463a9414243cc1a4b61dec9928704
shrutichanda <shrchand0@publicisgroupe.net>
2019-04-30 12:11:17 +0530

    eslint+prettier+vs code fixed 1. add es lint ext 2. configure prettier and eslint vscode settings

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+(function q() {
+	const age = '12';
+	return age;
+})();
+
+function add(a, b) {
+	return a + b;
+}
+
+const c = add(3, 5);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,14 @@
-/** 
- * Authored by Shruti 
+/**
+ * Authored by Shruti
  * Webpack configurations for module bundling
  */
 
-// Dependencies
 const path = require('path');
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        filename: "bundle.[name]-[contenthash].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.(js|scss|css)$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    quotes: ["error", "single"],
-                    "comma-style": [2, "last"]
-                }
-            }
-        ]
-    }
+	entry: './src/js/index.js',
+	output: {
+		filename: 'bundle.[name]-[contenthash].js',
+		path: path.resolve(__dirname, 'dist')
+	}
 };
