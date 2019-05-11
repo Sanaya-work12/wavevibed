e2c35eb6622fcc63f86937f170c0483031a9cdda
shrutichanda <shrchand0@publicisgroupe.net>
2019-05-11 11:48:43 +0530

    webpack dependencies added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import '../scss/index.scss';
+
 (function q() {
 	const age = '12';
 	return age;
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@
  * Authored by Shruti
  * Webpack configurations for module bundling
  */
-
+var webpack = require('webpack');
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const AutoPrefixer = require('autoprefixer');
 
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
-		filename: 'bundle.[name]-[contenthash].js',
+		filename: '[name].bundle-[contenthash].js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		inline: true,
+		open: true,
+		hot: true
 	},
 	module: {
 		rules: [
@@ -31,11 +37,30 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.scss$/,
+				test: /\.(scss|css)$/,
 				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+						  hmr: process.env.NODE_ENV === 'development',
+						}
+					},
+					'postcss-loader',
 					'style-loader', // creates style nodes from JS strings
 					'css-loader', // translates CSS into CommonJS
-					'sass-loader' // compiles Sass to CSS, using Node Sass by default
+					'sass-loader'// compiles Sass to CSS, using Node Sass by default
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
 				]
 			}
 		]
@@ -43,11 +68,12 @@ module.exports = {
 	plugins: [
 		new HtmlWebPackPlugin({
 			template: './src/index.html',
-			filename: './index.html'
+			filename: './src/index.html'
 		}),
 		new MiniCssExtractPlugin({
-			filename: "[name].css",
-      		chunkFilename: "[id].css"
-		})
+			filename: 'styles/[name].css',
+      		chunkFilename: '[id].css'
+		}),
+		new AutoPrefixer()
 	]
 };
